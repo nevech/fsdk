9382474e10d317418183c8f3b12cda40fe86d5c5
nevech <r.nevech@gmail.com>
2015-11-08 19:16:06 +0200

    remove auto run compile

--- index.js
+++ index.js
@@ -2,18 +2,14 @@ var vfs = require('vinyl-fs');
 var extend = require('util')._extend;
 var parser = require('./lib/parser')
 
-module.exports.parse = parser;
+var fsdk = module.exports.parse = {
+  parse: parser
+};
 
-module.exports.compile = function (options) {
+fsdk.compile = function (options) {
   var options = extend({}, options);
 
-  vfs.src(options.glob)
+  vfs.src(options.src)
     .pipe(parser(options))
     .pipe(vfs.dest(options.dest));
 };
-
-module.exports.compile({
-  glob: './test_src/*.*',
-  dest: './dist_src',
-  mode: 'admin'
-});
